1a6fe0c0325a607021c74f1fc1fcf77ceacc3135
Julian Olsen <46683927+JulianOlsen@users.noreply.github.com>
2019-06-12 18:37:31 +0200

    Add files via upload

---
+++ homework8c/src/homework8c/Homework8c.java
@@ -0,0 +1,32 @@
+package homework8c;
+
+import java.util.Scanner;
+
+public class Homework8c {
+
+    public static void main(String[] args) {
+        System.out.println("type any number and get the times table up to ten");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = n*1;
+        System.out.println("1*"+n+"="+a);
+        int b =n*2;
+        System.out.println("2*"+n+"="+b);
+        int c =n*3;
+        System.out.println("3*"+n+"="+c);
+        int d =n*4;
+        System.out.println("4*"+n+"="+d);
+        int e =n*5;
+        System.out.println("5*"+n+"="+e);
+        int f =n*6;
+        System.out.println("6*"+n+"="+f);
+        int g =n*7;
+        System.out.println("7*"+n+"="+g);
+        int h =n*8;
+        System.out.println("8*"+n+"="+h);
+        int i =n*9;
+        System.out.println("9*"+n+"="+i);
+        int j =n*10;
+        System.out.println("10*"+n+"="+j);       
+    }
+} 
